a6453a5ded0ed167c49b1472d835ddb3db39ccab
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2021-11-04 23:35:13 -0300

    Commit 0.0

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,10 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    testaComportamentosConta()
+    val sergio = Funcionario(nome = "Sergio", cpf = "111.222.333-44", salario = 1000.0)
 
+    println("Nome: ${sergio.nome}")
+    println("CPF: ${sergio.cpf}")
+    println("Salario: ${sergio.salario}")
+    println("Bonificação: ${sergio.bonificacao()}")
 }
