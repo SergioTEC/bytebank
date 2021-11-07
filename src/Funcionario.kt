e62f4869aedfd066fa6d2beea2782831348c2639
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2021-11-07 00:44:54 -0300

    Criando novas classes

--- src/Funcionario.kt
+++ src/Funcionario.kt
@@ -1,9 +1,9 @@
 class Funcionario(
     val nome: String,
     val cpf: String,
-    val salario: Double
-){
-    fun bonificacao(): Double{
+    val salario: Double,
+) {
+    fun bonificacao(): Double {
         return salario * 0.1
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,34 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    val sergio = Funcionario(nome = "Sergio", cpf = "111.222.333-44", salario = 1000.0)
+    val sergio = Funcionario(
+        nome = "Sergio",
+        cpf = "111.222.333-44",
+        salario = 1000.0
+    )
 
     println("Nome: ${sergio.nome}")
     println("CPF: ${sergio.cpf}")
     println("Salario: ${sergio.salario}")
     println("Bonificação: ${sergio.bonificacao()}")
+    println()
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "222.333.444-55",
+        salario = 2000.0,
+        senha = 1234
+
+    )
+
+    println("Nome: ${fran.nome}")
+    println("CPF: ${fran.cpf}")
+    println("Salario: ${fran.salario}")
+    println("Bonificação: ${fran.bonificacao()}")
+
+    if (fran.autenticacao(senha = 1234)){
+        println("Autenticado com sucesso")
+    } else {
+        println("Não autenticado")
+    }
 }
