f32f8ee2a28f7ec090719e9099773e89b17dc15a
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2022-01-20 21:18:01 -0300

    Variáveis globais e objects

--- src/br/com/sergio/bytebank/modelos/Conta.kt
+++ src/br/com/sergio/bytebank/modelos/Conta.kt
@@ -7,6 +7,15 @@ abstract class Conta(
     var saldo = 0.0
         protected set
 
+    companion object Contador{
+        var totalContas = 0
+            private set
+    }
+
+    init {
+        totalContas++
+    }
+
     fun deposita(valor: Double) {
         if (valor > 0) {
             this.saldo += valor
