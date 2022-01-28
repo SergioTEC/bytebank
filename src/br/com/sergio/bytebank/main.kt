a7862a2e716c671ac555ed1dc07e907b52ed4cdd
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2022-01-27 23:26:15 -0300

    Reutilizando e sobrescrevendo métodos de Any

--- src/br/com/sergio/bytebank/main.kt
+++ src/br/com/sergio/bytebank/main.kt
@@ -1,2 +1,14 @@
+import br.com.sergio.bytebank.modelos.Endereco
+
 fun main() {
+    val endereco = Endereco(logradouro = "Rua Treze de Maio", numero = 43, bairro = "Vila Europa", complemento = "Ap", cep = "94027")
+    val novoEndereco = Endereco(logradouro = "Rua Treze de Maio", numero = 43, bairro = "Vila Europa", complemento = "Ap", cep = "94027")
+
+    println(endereco.equals(novoEndereco))
+
+    println(endereco.toString())
+    println(novoEndereco)
+
+    println(endereco.hashCode())
+    println(novoEndereco.hashCode())
 }
