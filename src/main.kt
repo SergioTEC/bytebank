36f238a3470670b4aa2b27cd51bea14acd8b88c4
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2022-08-29 23:45:44 -0300

    Controle de fluxo e o paradigma procedural

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,43 @@
-fun main(){
-    print("Bem vindo ao Bytebank")
+fun main() {
+    println("Bem vindo ao Bytebank")
+
+    var i = 0
+    while (i < 5) {
+        val titular = "Sergio $i"
+        val numeroConta = 1000 + i
+        var saldo = i + 10
+
+        println("Titular: $titular")
+        println("Numero da conta: $numeroConta")
+        println("Saldo da conta: $saldo")
+        println()
+        i++
+    }
+//    for (i in 5 downTo 1 step 2) {
+//        val titular = "Sergio $i"
+//        val numeroConta = 1000 + i
+//        var saldo = i + 10
+//
+//        println("Titular: $titular")
+//        println("Numero da conta: $numeroConta")
+//        println("Saldo da conta: $saldo")
+//        println()
+//    }
+}
+
+fun testaCondicoes(saldo: Double) {
+
+    if (saldo > 0.0) {
+        println("Conta Positiva")
+    } else if (saldo == 0.0) {
+        println("Conta Neutra")
+    } else {
+        println("Conta Negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("Conta Positiva")
+        saldo == 0.0 -> println("Conta Neutra")
+        else -> println("Conta Negativa")
+    }
 }
